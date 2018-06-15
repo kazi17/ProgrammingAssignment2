160aec9b6ffdd351ec648eef16d81fa5599c3ba8
kazi17 <40235759+kazi17@users.noreply.github.com>
2018-06-15 23:16:23 +0600

    Add files via upload

---
+++ assignment-2.R
@@ -0,0 +1,32 @@
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+            x <<- y
+            inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+}
+mat <- makeCacheMatrix()
+
+## set the matrix value
+mat$set(matrix(data = (1:10), nrow = 5, ncol = 2))
+
+## Check that we stored it correctly
+mat$get()
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
